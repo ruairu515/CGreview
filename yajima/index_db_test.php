6fb9bcdf0b07c885b9f0c1e0033e7b069cd57d4b
矢嶋拓弥 <YajimaTakuya@yajimatakumi-no-MacBook-Pro.local>
2016-11-16 00:35:50 +0900

    index_db_testコメント削除

--- yajima/index_db_test.php
+++ yajima/index_db_test.php
@@ -1,33 +1,4 @@
 <?php
-//mysql_queryの実装
-    // // MySQLへ接続する準備。DB名や認証に必要な情報を格納
-    // $url = "mysql462.db.sakura.ne.jp";
-    // $user = "citnn";
-    // $pass = "ilovech11";
-    // $db = "citnn_cgpeer";
-    // $table = "posttest01";
-
-    // // MySQLへ接続する
-    // $connect = mysql_connect($url, $user, $pass) or die("MySQLへの接続に失敗しました。");
-
-    // // 手を加えるデータベースを選択する
-    // $sdb = mysql_select_db($db, $connect) or die("データベースの選択に失敗しました。");
-      
-    // // POSTされたパラメータを受け取る
-    // $myInput1 = $_POST['word1'];
-    // $myInput2 = $_POST['word2'];
-    // $myInput3 = $_POST["word3"];
-    // echo "<p>{$myInput1}と{$myInput2}と{$myInput3}を格納しました。</p>";
-
-    // // クエリを送信する
-    // $sql = "INSERT INTO $table VALUES('$myInput3', '$myInput1', '$myInput2')";
-    // $result = mysql_query($sql, $connect) or die("クエリの送信に失敗しました。<br />SQL:".$sql);
-      
-    // // MySQLへの接続を閉じる
-    // mysql_close($connect) or die("MySQL切断に失敗しました。");
-
-
-
 
 //PDOの時の実装
 $dsn = 'mysql:dbname=CGreview;host=localhost;charset=utf8';
